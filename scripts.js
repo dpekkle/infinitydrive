3e1211b54b9badef83a538dabb814349f9811e3d
dpekkle <danielle.cerisier@gmail.com>
2016-02-07 07:04:12 +1100

    save tests

--- scripts.js
+++ scripts.js
@@ -574,7 +574,7 @@ function loadGame()
 function saveGame()
 {
 	level_text.text = "Level " + level + "    saved";
-	
+	clearCookie();
 	var cookiestring = "gold=" + gold + ";goldmod=" + goldmod + ";";
 	
 	//setCookie("gold", gold);
@@ -585,4 +585,16 @@ function saveGame()
 	//setCookie("foremanmod", foremanmod);
 	//setCookie("ship", ship);
 	//setCookie("shipmod", shipmod);
+}
+
+function clearCookie()
+{
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+    	var cookie = cookies[i];
+    	var eqPos = cookie.indexOf("=");
+    	var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    	document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
 }
